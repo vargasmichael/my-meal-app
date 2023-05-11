4900ecd2b937b897b559ef5247128981c5093623
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2023-05-11 10:33:57 -0600

    This is presentable, just needs styling

--- server/config.py
+++ server/config.py
@@ -10,7 +10,7 @@ from flask_session import Session
 
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SECRET_KEY'] = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_PERMANENT'] = False
